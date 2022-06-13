136023653a4e6f51af3eb2f4962285fcb322678d
JaredMathis <jared.mathis@native.consulting>
2022-06-13 17:03:49 -0400

    progress

--- index.mjs
+++ index.mjs
@@ -1,4 +1,6 @@
+import path from 'path';
 import fs from 'fs';
+import axios from 'axios'
 
 export function file_exists(file) {
     return fs.promises.access(file, fs.constants.F_OK)
@@ -24,3 +26,34 @@ export function object_to_array(o) {
 export async function file_read_json(file_path) {
     return JSON.parse(await fs.promises.readFile(file_path));
 }
+
+function path_dowloads_get() {
+    return './downloads/'
+} 
+
+export function http_save_get(url) {
+    let file_path = path.join(
+        path_dowloads_get(),
+        path_http_to_file(url));
+    return {
+        file_path,
+        url,
+        get: async () => {
+            if (!await file_exists(file_path)) {
+                await js_sleep(3000);
+
+                let response = await axios.get(url);
+                await fs.promises.writeFile(file_path, response.data);
+            }
+            let result = await fs.promises.readFile(
+                file_path, 'utf8');
+
+            return result;
+        }
+    };
+}
+
+function path_http_to_file(url) {
+    return string_replace_any(url, [':', '/'], '_');
+}
+  
